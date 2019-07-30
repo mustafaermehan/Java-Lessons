45f27733d121023e2676613b2cfebdeb8fa1d15a
mamilius <muhammedermehan@hotmail.com>
2019-07-30 17:06:50 +0300

    first project

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int a=4;
+        int b= 3+5;
+        int c=7;
+        
+        int toplam= a + 2*b + c;
+        
+        System.out.println("Toplam ="+toplam);
+        
+        
+    }
+    
+}
